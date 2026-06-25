f2e8c462e656e109b50a044630a37515d412352d
saramaneesha01-collab <saramaneesha01@gmail.com>
2026-06-25 22:04:35 +0530

    Add UserRepository to handle database operations

---
+++ src/main/java/com/uni/lostandfound/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.uni.lostandfound.repository;
+
+import com.uni.lostandfound.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
