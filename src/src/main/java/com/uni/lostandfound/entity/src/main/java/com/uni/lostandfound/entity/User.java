79242436c37121b4e5139824979c824add8c176c
saramaneesha01-collab <saramaneesha01@gmail.com>
2026-06-25 22:03:15 +0530

    Create User entity with validations

---
+++ src/src/main/java/com/uni/lostandfound/entity/src/main/java/com/uni/lostandfound/entity/User.java
@@ -0,0 +1,38 @@
+package com.uni.lostandfound.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Item> items;
+}
